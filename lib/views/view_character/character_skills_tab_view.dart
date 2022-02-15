f493efd87d9b24cf3c421a235083408dd66b32e0
Alexis Beaudoin <alexis.beaudoin06@gmail.com>
2022-02-15 20:42:56 +0100

    feat: skills with proficiency

--- lib/views/view_character/character_skills_tab_view.dart
+++ lib/views/view_character/character_skills_tab_view.dart
@@ -45,7 +45,6 @@ class CharacterSkillsTab extends StatelessWidget {
   }
 
   Column _skillFrames() {
-    print(characterData.proficiencies);
     return Column(
       children: <Widget>[
         Row(
@@ -54,21 +53,179 @@ class CharacterSkillsTab extends StatelessWidget {
             CharacterDataFrame(
               label: 'Acrobatics',
               value:
-                  '${characterData.proficiencies.contains(Proficiency.ACROBATICS) ? characterData.acrobatics + characterData.proficiencyBonus : characterData.acrobatics}',
+                  '${characterData.acrobatics + (characterData.proficiencies.contains(Proficiency.ACROBATICS) ? characterData.proficiencyBonus : 0)}',
               proficiency:
                   characterData.proficiencies.contains(Proficiency.ACROBATICS),
               roleColor: characterData.secondaryColor,
             ),
             CharacterDataFrame(
               label: 'Animal Handling',
-              value: '${characterData.animalHandling}',
+              value:
+                  '${characterData.animalHandling + (characterData.proficiencies.contains(Proficiency.ANIMAL_HANDLING) ? characterData.proficiencyBonus : 0)}',
+              proficiency: characterData.proficiencies
+                  .contains(Proficiency.ANIMAL_HANDLING),
+              roleColor: characterData.secondaryColor,
             ),
             CharacterDataFrame(
               label: 'Arcana',
-              value: '${characterData.arcana}',
-            )
+              value:
+                  '${characterData.arcana + (characterData.proficiencies.contains(Proficiency.ARCANA) ? characterData.proficiencyBonus : 0)}',
+              proficiency:
+                  characterData.proficiencies.contains(Proficiency.ARCANA),
+              roleColor: characterData.secondaryColor,
+            ),
           ],
-        )
+        ),
+        const SizedBox(height: margin.MARGIN_S),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <CharacterDataFrame>[
+            CharacterDataFrame(
+              label: 'Athletics',
+              value:
+                  '${characterData.athletics + (characterData.proficiencies.contains(Proficiency.ATHLETICS) ? characterData.proficiencyBonus : 0)}',
+              proficiency:
+                  characterData.proficiencies.contains(Proficiency.ATHLETICS),
+              roleColor: characterData.secondaryColor,
+            ),
+            CharacterDataFrame(
+              label: 'Deception',
+              value:
+                  '${characterData.deception + (characterData.proficiencies.contains(Proficiency.DECEPTION) ? characterData.proficiencyBonus : 0)}',
+              proficiency:
+                  characterData.proficiencies.contains(Proficiency.DECEPTION),
+              roleColor: characterData.secondaryColor,
+            ),
+            CharacterDataFrame(
+              label: 'History',
+              value:
+                  '${characterData.history + (characterData.proficiencies.contains(Proficiency.HISTORY) ? characterData.proficiencyBonus : 0)}',
+              proficiency:
+                  characterData.proficiencies.contains(Proficiency.HISTORY),
+              roleColor: characterData.secondaryColor,
+            ),
+          ],
+        ),
+        const SizedBox(height: margin.MARGIN_S),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <CharacterDataFrame>[
+            CharacterDataFrame(
+              label: 'Insight',
+              value:
+                  '${characterData.insight + (characterData.proficiencies.contains(Proficiency.INSIGHT) ? characterData.proficiencyBonus : 0)}',
+              proficiency:
+                  characterData.proficiencies.contains(Proficiency.INSIGHT),
+              roleColor: characterData.secondaryColor,
+            ),
+            CharacterDataFrame(
+              label: 'Intimidation',
+              value:
+                  '${characterData.intimidation + (characterData.proficiencies.contains(Proficiency.INTIMIDATION) ? characterData.proficiencyBonus : 0)}',
+              proficiency: characterData.proficiencies
+                  .contains(Proficiency.INTIMIDATION),
+              roleColor: characterData.secondaryColor,
+            ),
+            CharacterDataFrame(
+              label: 'Investigation',
+              value:
+                  '${characterData.investigation + (characterData.proficiencies.contains(Proficiency.INVESTIGATION) ? characterData.proficiencyBonus : 0)}',
+              proficiency: characterData.proficiencies
+                  .contains(Proficiency.INVESTIGATION),
+              roleColor: characterData.secondaryColor,
+            ),
+          ],
+        ),
+        const SizedBox(height: margin.MARGIN_S),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <CharacterDataFrame>[
+            CharacterDataFrame(
+              label: 'Medicine',
+              value:
+                  '${characterData.medicine + (characterData.proficiencies.contains(Proficiency.MEDICINE) ? characterData.proficiencyBonus : 0)}',
+              proficiency:
+                  characterData.proficiencies.contains(Proficiency.MEDICINE),
+              roleColor: characterData.secondaryColor,
+            ),
+            CharacterDataFrame(
+              label: 'Nature',
+              value:
+                  '${characterData.nature + (characterData.proficiencies.contains(Proficiency.NATURE) ? characterData.proficiencyBonus : 0)}',
+              proficiency:
+                  characterData.proficiencies.contains(Proficiency.NATURE),
+              roleColor: characterData.secondaryColor,
+            ),
+            CharacterDataFrame(
+              label: 'Perception',
+              value:
+                  '${characterData.perception + (characterData.proficiencies.contains(Proficiency.PERCEPTION) ? characterData.proficiencyBonus : 0)}',
+              proficiency:
+                  characterData.proficiencies.contains(Proficiency.PERCEPTION),
+              roleColor: characterData.secondaryColor,
+            ),
+          ],
+        ),
+        const SizedBox(height: margin.MARGIN_S),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <CharacterDataFrame>[
+            CharacterDataFrame(
+              label: 'Performance',
+              value:
+                  '${characterData.performance + (characterData.proficiencies.contains(Proficiency.PERFORMANCE) ? characterData.proficiencyBonus : 0)}',
+              proficiency:
+                  characterData.proficiencies.contains(Proficiency.PERFORMANCE),
+              roleColor: characterData.secondaryColor,
+            ),
+            CharacterDataFrame(
+              label: 'Persuasion',
+              value:
+                  '${characterData.persuasion + (characterData.proficiencies.contains(Proficiency.PERSUASION) ? characterData.proficiencyBonus : 0)}',
+              proficiency:
+                  characterData.proficiencies.contains(Proficiency.PERSUASION),
+              roleColor: characterData.secondaryColor,
+            ),
+            CharacterDataFrame(
+              label: 'Religion',
+              value:
+                  '${characterData.religion + (characterData.proficiencies.contains(Proficiency.RELIGION) ? characterData.proficiencyBonus : 0)}',
+              proficiency:
+                  characterData.proficiencies.contains(Proficiency.RELIGION),
+              roleColor: characterData.secondaryColor,
+            ),
+          ],
+        ),
+        const SizedBox(height: margin.MARGIN_S),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <CharacterDataFrame>[
+            CharacterDataFrame(
+              label: 'Sleight of Hands',
+              value:
+                  '${characterData.sleightOfHands + (characterData.proficiencies.contains(Proficiency.SLEIGHT_OF_HANDS) ? characterData.proficiencyBonus : 0)}',
+              proficiency: characterData.proficiencies
+                  .contains(Proficiency.SLEIGHT_OF_HANDS),
+              roleColor: characterData.secondaryColor,
+            ),
+            CharacterDataFrame(
+              label: 'Stealth',
+              value:
+                  '${characterData.stealth + (characterData.proficiencies.contains(Proficiency.STEALTH) ? characterData.proficiencyBonus : 0)}',
+              proficiency:
+                  characterData.proficiencies.contains(Proficiency.STEALTH),
+              roleColor: characterData.secondaryColor,
+            ),
+            CharacterDataFrame(
+              label: 'Survival',
+              value:
+                  '${characterData.survival + (characterData.proficiencies.contains(Proficiency.SURVIVAL) ? characterData.proficiencyBonus : 0)}',
+              proficiency:
+                  characterData.proficiencies.contains(Proficiency.SURVIVAL),
+              roleColor: characterData.secondaryColor,
+            ),
+          ],
+        ),
       ],
     );
   }
