379041ebea13c36be71aeaec0e7d92ca28b3d625
Alexis Beaudoin <alexis.beaudoin06@gmail.com>
2022-02-15 16:23:16 +0100

    feat-wip: proficiencies

---
+++ lib/core/constants/proficiency.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: constant_identifier_names
+
+enum Proficiency {
+  ACROBATICS, // 0
+  ANIMAL_HANDLING, // 1
+  ARCANA, // 2
+  ATHLETICS, // 3
+  DECEPTION, // 4
+  HISTORY, // 5
+  INSIGHT, // 6
+  INTIMIDATION, // 7
+  INVESTIGATION, // 8
+  MEDICINE, // 9
+  NATURE, // 10
+  PERCEPTION, // 11
+  PERFORMANCE, // 12
+  PERSUASION, // 13
+  RELIGION, // 14
+  SLEIGHT_OF_HANDS, // 15
+  STEALTH, // 16
+  SURVIVAL, // 17
+}
